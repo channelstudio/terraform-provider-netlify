2e7299ab0cdef6d8eb075ebdbc92b07c81e7769a
evan <kirkilese@gmail.com>
2023-01-08 06:24:01 -0500

    Added registry manifest.

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/terraform-providers/terraform-provider-netlify/netlify"
 )
 
+// Generate the Terraform provider documentation using `tfplugindocs`:
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: netlify.Provider})
